be4aedd08b995202fede659204aa9efe378ec22d
Anna Quaglieri <annaquaglieri16@gmail.com>
2020-11-18 12:44:33 +1100

    update patchwork

--- pick_names.R
+++ pick_names.R
@@ -1,9 +1,10 @@
-talk_names <- c("Koo", "Anna", "Song", "Sehrish", "Shazia", "Zeinab", "Adele", "Emi", "Alice", "Dianne")
+talk_names <- c("Koo", "Anna", "Song", "Sehrish", "Shazia", "Zeinab", "Emi", "Alice", "Dianne")
 
-set.seed(100)
+set.seed(200)
 sample_speaker <- sample(x = talk_names, size = 6, replace = FALSE)
-
+sample_speaker
 
 # Sample winners
-talk_names <- c("Koo", "Anna", "Song", "Sehrish", "Shazia", "Adele", "Emi", "Alice", "Dianne")
+talk_names <- c("Koo", "Song", "Sehrish", "Shazia", "Adele", "Emi", "Alice", "Dianne")
+sample_winner <- sample(x = talk_names, size = 1)
 
